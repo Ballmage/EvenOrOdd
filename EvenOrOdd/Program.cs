f3235a02b63fd85f768c110e1fa8d3e346c13716
Ballmage <mish2000@bk.ru>
2023-04-10 16:59:27 +0300

    Добавьте файлы проекта.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        // Первый вариант
+        public static string EvenOrOdd1(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+
+        // Второй вариант
+        public static string EvenOrOdd2(int number)
+        {
+
+            if (number % 2 == 0)
+            {
+                return ("Even");
+            }
+            else
+            {
+                return ("Odd");
+            }
+        }
+        // Третий вариант
+        public static string EvenOrOdd3(int number)
+         => number % 2 == 0 ? "Even" : "Odd";
+
+        // Четвертый вариант
+        public static Func<int, string> EvenOrOdd4 = (x) => x % 2 == 0 ? "Even" : "Odd";
+
+        // Пятый вариант
+        public static string EvenOrOdd5(int number)
+        {
+            return new string[] { "Even", "Odd" }[number % 2];
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
